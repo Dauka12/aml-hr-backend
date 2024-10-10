5854d71beeaaf3189babeabe7945115e83ee5db0
Daulet Akhythan <93545836+Dauka12@users.noreply.github.com>
2024-10-10 20:03:40 +0600

    Update Task.java

--- src/main/java/com/example/models/Task.java
+++ src/main/java/com/example/models/Task.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+//TODO сделать таски в базе более подходящим позже
 @Entity
 @Table(name = "tasks")
 public class Task {
